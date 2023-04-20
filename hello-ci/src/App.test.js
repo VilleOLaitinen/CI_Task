81ef90cb6022408924d407657053997c45da9b18
Ville Laitinen <vlaitine@dc.lab>
2023-04-20 14:27:57 +0300

    testing workflow and tests added

---
+++ hello-ci/src/App.test.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { render, screen } from '@testing-library/react'
+import App from './App'
+
+test('App component renders the title "Hello CI/CD!"', () => {
+    render(<App />)
+    
+    const titleShown = screen.getByText('Hello CI/CD!')
+
+    expect(titleShown).toBeDefined()
+})
+
+test('App component renders the text "Let`s test some GitHub Actions"', () => {
+    render(<App />)
+
+    const textShown = screen.getByText("Let's test some GitHub Actions")
+    
+    expect(textShown).toBeDefined()
+})
