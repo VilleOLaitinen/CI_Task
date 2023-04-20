d91369a26d051a38c35c361cf72b2a84c19b4802
Ville Laitinen <vlaitine@dc.lab>
2023-04-20 15:28:36 +0300

    this commit def should not pass tests!

--- hello-ci/src/App.js
+++ hello-ci/src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div>
       <h2>Hello Hello Hello!</h2>
-      <p>This def shouldn't pass tests</p>
+      <p>This def shouldn't pass tests!</p>
     </div>
   );
 }
