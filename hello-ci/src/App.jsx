f4c4a1720a77a1b569df950835c922a1a8dbf1d5
Ville Laitinen <ville.o.laitinen@edu.turkuamk.fi>
2023-04-21 16:59:24 +0300

    linting added

--- hello-ci/src/App.jsx
+++ hello-ci/src/App.jsx
@@ -1,7 +1,10 @@
+import React from 'react';
+
 function App() {
   return (
     <div>
       <h2>Hello CI/CD!</h2>
+      {/* eslint-disable-next-line react/no-unescaped-entities */}
       <p>Let's test some GitHub Actions</p>
     </div>
   );
